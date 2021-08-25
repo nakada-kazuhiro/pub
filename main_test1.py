f30a8cd2ca8ca3fd298161d3ea32c244cae14e12
miyaji.nakada@gmail.com <miyaji.nakada@gmail.com>
2021-08-25 13:27:55 +0900

    テスト2

--- main_test1.py
+++ main_test1.py
@@ -4,22 +4,23 @@ import pandas as pd
 from PIL import Image
 import time
 
-st.sidebar.title('streamlit 超入門')
+st.sidebar.title('アップロードファイル集計')
 
 # メイン画面
 st.header('読み込みデータ表示')
 
 
-st.title('Explore a dataset')
-st.write('A general purpose data exploration app')
-file = st.file_uploader("Upload file", type=['csv'])
-st.write(file)
 
 
 
 # サイドバー
 # ファイルアップロード
 uploaded_file = st.sidebar.file_uploader("ファイルアップロード", type='csv') 
+tbl1 = st.sidebar.checkbox('データ表示', value=False) 
+tbl2 = st.sidebar.checkbox('担当者別計表示', value=False) 
+
+
+
 
 if uploaded_file is not None:
     # アップロードファイルをメイン画面にデータ表示
@@ -27,7 +28,8 @@ if uploaded_file is not None:
     df = df.iloc[:,[3, 4, 9, 10, 11, 12, 14, 17, 18, 19, 20, 26]]
     df = df.dropna()
     df = df.reset_index(drop=True)
-    st.write(df)
+    if tbl1:
+	    st.write(df)
     members = df['担当者名'].unique()
     
     total_tbl = []
@@ -39,10 +41,18 @@ if uploaded_file is not None:
     total_gaku = df['納入合計'].str.replace(',','').astype(int).sum()
     total_gaku_h = '{:,}'.format(total_gaku)
     total_tbl.append(dict(担当者名= '事業所計', 納入合計 = total_gaku_h))
-    
     df_display = pd.DataFrame(total_tbl, columns=['担当者名', '納入合計'])
-    df_display
-    
+    if tbl2:
+    	' '
+    	st.header('担当者別計')
+    	st.write(df_display)
+    	st.header('グラフ')
+    	df_display = df_display.set_index('担当者名')
+    	st.line_chart(df_display)
+    	st.write(df_display)
+
+
+
 
 left_column, right_column = st.columns(2)
 button = left_column.button('右カラムに文字を表示')
